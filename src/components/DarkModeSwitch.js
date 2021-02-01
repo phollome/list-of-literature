df5a0d9c34b83a84d8329ca92ea6c70c78a1ab4c
Peter Holló <lordpejdar@gmail.com>
2021-02-01 12:51:27 +0100

    Use hooks package

--- src/components/DarkModeSwitch.js
+++ src/components/DarkModeSwitch.js
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { useDarkMode } from "../hooks";
+import { useDarkMode } from "@phollome/react-hooks";
 
 function DarkModeSwitch() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
